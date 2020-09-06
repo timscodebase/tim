3371c8390b9fcce03747d788c3db6e175c4535f7
Tim Smith <tim.smith.hdg@gmail.com>
2020-09-06 08:41:49 -0700

    Message

--- pages/index.js
+++ pages/index.js
@@ -40,12 +40,11 @@ export default function Home() {
       <main>
         <MainContent>
           <p className="padding">
-            My passion in life is meeting great new people and learning as much
-            as I can about the many things I love! Every day, I find new
-            inspiring things that motivate me to keep moving forward in life and
-            to continue my search for my purpose in life. I believe that my
-            Higher Power put me on this Earth to learn and inspire people to
-            love life.
+            My passion in life is meeting new people and learning as much as I
+            can about the many things I love! Every day, I find new inspiring
+            things that motivate me to keep moving forward and to continue my
+            search for my purpose. I believe that my Higher Power put me on this
+            Earth to learn and inspire people to love life as I do.
           </p>
 
           <DiagonalContent id="projects">
