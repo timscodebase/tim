e46dd5adf4ca7d8f322ea31400cd37820f5312b9
Tim Smith <tim.smith.hdg@gmail.com>
2020-09-16 03:39:37 -0700

    test

--- pages/_app.js
+++ pages/_app.js
@@ -9,24 +9,24 @@ import Layout from "./templates/Layout";
 export default function MyApp({ Component, pageProps }) {
   const [theme, setTheme] = useState("dark");
 
-  const startTime = "08:00:00";
-  const endTime = "20:00:00";
+  // const startTime = "08:00:00";
+  // const endTime = "20:00:00";
 
-  const currentDate = new Date();
+  // const currentDate = new Date();
 
-  const startDate = new Date(currentDate.getTime());
-  startDate.setHours(startTime.split(":")[0]);
-  startDate.setMinutes(startTime.split(":")[1]);
-  startDate.setSeconds(startTime.split(":")[2]);
+  // const startDate = new Date(currentDate.getTime());
+  // startDate.setHours(startTime.split(":")[0]);
+  // startDate.setMinutes(startTime.split(":")[1]);
+  // startDate.setSeconds(startTime.split(":")[2]);
 
-  const endDate = new Date(currentDate.getTime());
-  endDate.setHours(endTime.split(":")[0]);
-  endDate.setMinutes(endTime.split(":")[1]);
-  endDate.setSeconds(endTime.split(":")[2]);
+  // const endDate = new Date(currentDate.getTime());
+  // endDate.setHours(endTime.split(":")[0]);
+  // endDate.setMinutes(endTime.split(":")[1]);
+  // endDate.setSeconds(endTime.split(":")[2]);
 
-  if (startDate < currentDate && endDate > currentDate) {
-    setTheme("light");
-  }
+  // if (startDate < currentDate && endDate > currentDate) {
+  //   setTheme("light");
+  // }
 
   const context = { theme, setTheme };
   return (
