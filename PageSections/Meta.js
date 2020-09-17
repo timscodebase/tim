53038fc957595da3b4c0304915b1756f3d284694
Tim Smith <tim.smith.hdg@gmail.com>
2020-09-16 22:36:45 -0700

    analytics

--- PageSections/Meta.js
+++ PageSections/Meta.js
@@ -99,6 +99,14 @@ export default function Meta() {
       })(window,document,'script','dataLayer','GTM-NKXQMDJ');`,
       }}>
       </script>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-178247022-1"></script>
+      <script dangerouslySetInnerHTML={{
+        __html: `window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'UA-178247022-1');`}}>
+      </script>
+
     </Head>
   );
 }
